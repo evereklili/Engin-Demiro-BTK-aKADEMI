bf5fcece114e74b757691630fa415b4a873b8479
OMER DOGU - www.omerdogu.com <omerdogu@omerdogu.com>
2022-01-26 12:27:28 +0300

    Ömer DOĞU UpdateAdd project files.

---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        private readonly object dgwProducts;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            using (ETradeContext context = new ETradeContext()) //
+            {
+                //using şu anlama gelir ETradeContext pahalı bir nesnedir çok yer kaplıyordur. Bu bağlamda bu tip nesneelere f12 gittiğimde 
+                //IDisposeble interface implemente edilmektedir. bunu yapınca şunu sağlamış oluruz? method bittiği zaman .net memory yöneticisi garbage collector bunu bellekten atmaya başlıyor. 
+                //çöptoplayıcı ile yapar. using kullanmanın avantajı blok bittiği anda garbage collector beklemeden dispose (bellekten atıyoruz.) 
+
+               // dgwProducts.DataSource = context.Products.ToList();
+
+
+            }
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
---
+++ WindowsFormsApp1/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public decimal UnitPrice { get; set; }
+        public int StockAmount { get; set; }
+       //namespace bizim gruplama tekniği olarak kullanılır. 
+       //en önemli kurual; tabloya karşılık gelen bir class olmasıdır. biz entity framework 
+       //bizim bir klasımız var sql şuraya karşılık gelmektedir. Context(UnitifWork) tasarım deseninini çağırırız. 
+
+    }
+}
